32d06971f14e141dd675d6bfeeea78a60b38c7bd
Sergio Smirnoff <ssmirnoff@itba.edu.ar>
2026-04-20 23:45:26 -0300

    fix(investments): add missing Feign dependency and @EnableFeignClients annotation

--- src/main/java/com/financialapp/investments/InvestmentsApplication.java
+++ src/main/java/com/financialapp/investments/InvestmentsApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
 @ConfigurationPropertiesScan
+@EnableFeignClients
 public class InvestmentsApplication {
 
     public static void main(String[] args) {
